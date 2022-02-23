103ffae6354d439324a352a3f2d61639b584afb2
aaronb05 <aboyles05@gmail.com>
2022-02-23 13:20:47 -0500

    css overhaul, logo and contact form, new stock redirect pages for email results

--- server.py
+++ server.py
@@ -23,6 +23,33 @@ def elements():
     return render_template('elements.html')
 
 
+@app.route('/send_email', methods=["POST"])
+def send_email():
+    from_email = request.form["email"]
+    email_message = request.form["message"]
+    user = request.form["name"]
+    smtp_email = os.getenv("my_email")
+    smtp_pass = os.getenv("password")
+    if from_email == "" or user == "" or email_message == "" or request.form["human"] is None:
+        return render_template("email_failure.html", email=from_email, name=user, message=email_message)
+    else:
+        try:
+            with SMTP("smtp.gmail.com", 587) as connection:
+                connection.starttls()
+                connection.login(user=smtp_email, password=smtp_pass)
+                connection.sendmail(
+                    from_addr=from_email,
+                    to_addrs="aboyles05@gmail.com",
+                    msg=f"Subject: New Inquiry from {user} at {from_email}!\n\n  {email_message}")
+        except SMTPResponseException as e:
+            error_message = f"Sorry we could not complete the request due to{e.smtp_error}"
+            return error_message
+        else:
+            message = "We will be in touch with you soon, " \
+                      "please allow 3 business days for us to respond"
+            return render_template("send_email_success.html", result=message)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
