9b6c3dfdbd670dd7d376a2ec112599f79d5b36ac
aaronb05 <aboyles05@gmail.com>
2022-02-23 16:58:37 -0500

    update email success page

--- server.py
+++ server.py
@@ -28,9 +28,10 @@ def send_email():
     from_email = request.form["email"]
     email_message = request.form["message"]
     user = request.form["name"]
+    category = request.form["category"]
     smtp_email = os.getenv("my_email")
     smtp_pass = os.getenv("password")
-    if from_email == "" or user == "" or email_message == "" or request.form["human"] is None:
+    if from_email == "" or user == "" or email_message == "":
         return render_template("email_failure.html", email=from_email, name=user, message=email_message)
     else:
         try:
@@ -40,14 +41,16 @@ def send_email():
                 connection.sendmail(
                     from_addr=from_email,
                     to_addrs="aboyles05@gmail.com",
-                    msg=f"Subject: New Inquiry from {user} at {from_email}!\n\n  {email_message}")
+                    msg=f"Subject: New Inquiry from {user} at {from_email}!\n\n  "
+                        f"Category: {category}\n"
+                        f"{email_message}")
         except SMTPResponseException as e:
             error_message = f"Sorry we could not complete the request due to{e.smtp_error}"
             return error_message
         else:
             message = "We will be in touch with you soon, " \
                       "please allow 3 business days for us to respond"
-            return render_template("send_email_success.html", result=message)
+            return render_template("email_success.html", result=message)
 
 
 if __name__ == "__main__":
